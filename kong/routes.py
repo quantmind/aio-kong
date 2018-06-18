e9c287d517713e6114c7b1a9da6d119e883191eb
Luca Sbardella <luca@quantmind.com>
2018-06-18 10:52:49 +0100

    route plugins

--- kong/routes.py
+++ kong/routes.py
@@ -1,12 +1,16 @@
 from itertools import zip_longest
 
-from .components import ServiceEntity
+from .components import ServiceEntity, KongEntity
 from .utils import as_list
+from .plugins import RoutePlugins
 
 
 class ServiceRoutes(ServiceEntity):
     """Routes associates with a service
     """
+    def wrap(self, data):
+        return Route(self, data)
+
     def create(self, skip_error=None, **params):
         params['service'] = dict(id=self.root.id)
         return self.execute(self.url, 'post', json=params,
@@ -20,14 +24,25 @@ class ServiceRoutes(ServiceEntity):
         for d, route in zip_longest(data, routes):
             if not d:
                 if route:
-                    await self.delete(route['id'])
+                    await self.delete(route.id)
                 continue
+            plugins = d.pop('plugins', None)
             as_list('hosts', d)
             as_list('paths', d)
             as_list('methods', d)
             if not route:
                 route = await self.create(**d)
             else:
-                route = await self.update(route['id'], **d)
-            result.append(route)
+                route = await self.update(route.id, **d)
+            if plugins:
+                route.data['plugins'] = await route.plugins.apply_json(plugins)
+            result.append(route.data)
         return result
+
+
+class Route(KongEntity):
+    """Object representing a route
+    """
+    @property
+    def plugins(self):
+        return RoutePlugins(self, 'plugins')
